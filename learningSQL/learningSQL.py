37390ffa8e70c8682e1114aa3d3bb8a19346d05d
mettinger <jettinger35@gmail.com>
2015-11-07 13:59:52 -0800

    comment

--- learningSQL/learningSQL.py
+++ learningSQL/learningSQL.py
@@ -1,8 +1,43 @@
+#%%
+
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation
 from keras.layers.embeddings import Embedding
 from keras.layers.recurrent import LSTM
 
+import numpy as np
+import pandas as pd
+from pandasql import sqldf
+
+#%%
+
+# generate a random table of integers
+def randomTable(numRows, numCols, numItems, columnNames):
+    return pd.DataFrame(np.random.randint(0, numItems, size = (numRows,numCols)), columns = columnNames, dtype=int)
+    
+# do sql query on pandas dataframe
+def doQuery(query):
+    return sqldf(query, globals())
+    
+    
+#%% generate random tables and query results
+    
+query = "select col0 from thisTable where col1 = 0"
+numSamples = 10
+tableRows = 10
+tableColumns = 10
+numItems = 20
+columnNames = ["col" + str(i) for i in range(tableColumns)]
+X_train = np.zeros((numSamples, tableColumns * tableRows), dtype = int)
+for i in range(numSamples):
+    thisTable = randomTable(tableRows, tableColumns, numItems, columnNames)
+    X_train[i,:] = thisTable.values.flatten()
+    queryResult = doQuery(query).values.flatten().astype(int)
+    print queryResult
+    
+    
+    
+#%%
 model = Sequential()
 model.add(Embedding(max_features, 256, input_length=maxlen))
 model.add(LSTM(output_dim=128, activation='sigmoid', inner_activation='hard_sigmoid'))
