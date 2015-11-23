c9a70ebeb5bc7e406e8aee96a3fc1f72c29ba8d0
mettinger <jettinger35@gmail.com>
2015-11-23 10:02:50 -0800

    comment

--- sphereAndCube/sphereAndCube.py
+++ sphereAndCube/sphereAndCube.py
@@ -14,16 +14,21 @@ f.close()
 
 dataShuffled = sk.utils.shuffle(data)
 
-#%% display random image
-
-testImage = data[12010,1:]
-testImage2 = np.reshape(testImage,(40,40))
-plt.imshow(testImage2)
-
 #%%
 
 model = svm.LinearSVC()
 
 #%%
 
-scores = cross_validation.cross_val_score(model, dataShuffled[:,1:],dataShuffled[:,0])
+scores = cross_validation.cross_val_score(model, dataShuffled[:,1:],dataShuffled[:,0],cv=5)
+print "scores: " + str(scores)
+
+#%% display random image
+
+doFlag = 0
+if doFlag == 1:
+    testImage = data[12021,1:]
+    testImage2 = np.reshape(testImage,(40,40))
+    plt.imshow(testImage2)
+    
+    
