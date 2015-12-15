9a44d694b1f06acee41507197dfa260a9ae63e4f
mettinger <jettinger35@gmail.com>
2015-12-15 11:04:34 -0800

    comment

--- sphereAndCube/sphereAndCube.py
+++ sphereAndCube/sphereAndCube.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import sklearn as sk
 from sklearn import svm
 from sklearn import cross_validation
+import random
 
 #%% READ THE DATA
 
@@ -22,6 +23,19 @@ model = svm.LinearSVC()
 
 scores = cross_validation.cross_val_score(model, dataShuffled[:,1:],dataShuffled[:,0],cv=5)
 print "scores: " + str(scores)
+
+#%%
+model.fit(dataShuffled[:,1:],dataShuffled[:,0])
+
+#%%
+
+sampleIndex = random.randint(0,dataShuffled.shape[0])
+
+print "Sample: " + str(sampleIndex) + " True: " + str(dataShuffled[sampleIndex,0]) \
+        + " Predicted: " + str(model.predict(dataShuffled[sampleIndex,1:].reshape(1,-1)))
+        
+coeff = model.coef_
+intercept = model.intercept_
 
 #%% display random image
 
