8f3e27512be63aebd2a284dc5e7071be89821d81
JRS <Jrsteeves@live.com>
2016-07-29 23:56:44 -0700

    to pandas groupings

--- poloniex/poloniexDAO.py
+++ poloniex/poloniexDAO.py
@@ -18,8 +18,7 @@ class PoloniexDAO():
 			try:
 				self.__conn = sqlite3.connect( self.db_path )
 				self.__cursor = self.__conn.cursor()
-				self.__createTables()
-				self.update()
+				self.createTables()
 
 
 			except sqlite3.Error as detail:
@@ -31,67 +30,93 @@ class PoloniexDAO():
 				raise IOError('Directory not found: ' + directory)
 
 	def selectMaxTime(self):
-		self.__createTables()
-		SQL_MIN = "SELECT max(date) FROM PRICE_1MIN"
+		self.createTables()
+		SQL_MIN = "SELECT max(date) FROM PRICE"
 		self.__cursor.execute( SQL_MIN )
 		return self.__cursor.fetchall()[0][0]
 
 	def selectMinTime(self):
-		self.__createTables()
-		SQL_MIN = "SELECT min(date) FROM PRICE_1MIN"
+		self.createTables()
+		SQL_MIN = "SELECT min(date) FROM PRICE"
 		self.__cursor.execute( SQL_MIN )
 		return self.__cursor.fetchall()[0][0]
 
 	def selectAllTradeHistory(self):
-		self.__createTables()
+		self.createTables()
 		SQL_SELECT_ALL = 'SELECT * FROM TRADES ORDER BY tradeID'
 		self.__cursor.execute( SQL_SELECT_ALL )
 		all_rows = self.__cursor.fetchall()
-		return all_rows
+		return pandas.DataFrame( all_rows, columns=["amount", "date", "globaleTradeID", "rate", "total", "tradeID", "type"]  )
 
-	def selectAllPrice1MinHistory(self):
-		self.__createTables()
-		SQL_SELECT_ALL = 'SELECT * FROM PRICE_1MIN ORDER BY date'
+	def selectTradeHistory(self, startTime, endTime):
+		self.createTables()
+		SQL_SELECT_ALL = 'SELECT * FROM TRADES WHERE date BETWEEN ? AND ? ORDER BY tradeID'
+		self.__cursor.execute( SQL_SELECT_ALL, [startTime, endTime]  )
+		all_rows = self.__cursor.fetchall()
+		return pandas.DataFrame( all_rows, columns=["amount", "date", "globaleTradeID", "rate", "total", "tradeID", "type"]  )
+
+	def selectAllPriceHistory(self):
+		self.createTables()
+		SQL_SELECT_ALL = 'SELECT * FROM PRICE ORDER BY date'
 		self.__cursor.execute( SQL_SELECT_ALL )
 		all_rows = self.__cursor.fetchall()
 		return pandas.DataFrame( all_rows, columns=["date", "open", "high", "low", "close", "volume"]  )
 
-	def selectPrice1MinHistory(self, startTime, endTime):
-	
-		self.__createTables()
-		if type(startTime) == str:
-        	startTime = int(time.mktime(time.strptime(startTime, "%Y-%m-%d %H:%M:%S")))
-        
-    	if type(endTime) == str:
-        	endTime = int(time.mktime(time.strptime(endTime, "%Y-%m-%d %H:%M:%S")))
-
-		SQL_SELECT = 'SELECT * FROM PRICE_1MIN WHERE date BETWEEN ? AND ?'
+	def selectPriceHistory(self, startTime, endTime):
+		self.createTables()
+		SQL_SELECT = 'SELECT * FROM PRICE WHERE date BETWEEN ? AND ? ORDER BY date'
 		self.__cursor.execute( SQL_SELECT, [startTime, endTime] )
 		all_rows = self.__cursor.fetchall()
 		return pandas.DataFrame( all_rows, columns=["date", "open", "high", "low", "close", "volume"]  )
 
-	def clear(self):
+	def dropTables(self):
 		DROP_TRADES_TABLE = "DROP TABLE IF EXISTS TRADES"
-		DROP_PRICE_1MIN_TABLE = "DROP TABLE IF EXISTS PRICE_1MIN"
+		DROP_PRICE_TABLE = "DROP TABLE IF EXISTS PRICE"
 		self.__cursor.execute( DROP_TRADES_TABLE )
-		self.__cursor.execute( DROP_PRICE_1MIN_TABLE )
+		self.__cursor.execute( DROP_PRICE_TABLE )
+
+	def createTables(self):
+		CREATE_TRADES_TABLE = 'CREATE TABLE IF NOT EXISTS TRADES ("amount" DOUBLE NOT NULL , "date" DATETIME NOT NULL ,  "globalTradeID" INTEGER PRIMARY KEY  NOT NULL  UNIQUE , "rate" DOUBLE NOT NULL , "total" DOUBLE NOT NULL , "tradeID" INTEGER NOT NULL  UNIQUE , "type" BOOL NOT NULL )'
+		CREATE_PRICE_TABLE = 'CREATE TABLE IF NOT EXISTS PRICE ("date" INT PRIMARY KEY NOT NULL UNIQUE, "open" DOUBLE NOT NULL , "high" DOUBLE NOT NULL , "low" DOUBLE NOT NULL, "close" DOUBLE NOT NULL , "volume" DOUBLE NOT NULL)'
+		self.__cursor.execute( CREATE_TRADES_TABLE )
+		self.__cursor.execute( CREATE_PRICE_TABLE )
 
 
 	def update(self):
 
-		s = self.selectMaxTime()
-		if(s == None):
-			s = int(time.mktime(time.strptime('2016-07-29 21:00:00', "%Y-%m-%d %H:%M:%S")))
-		results = poloniexClient.pullTradeHistory(s , int(time.time()), self.currencyPair)
+		startTime = self.selectMaxTime()
+		endTime = int(time.time())
+		if(startTime == None):
+			startTime = '2016-07-29 21:00:00'
+		startTime = toSeconds(startTime)
+
+		results = self.__pullTradeHistory( startTime, endTime )
 		self.__updateTrades(results)
-		self.__updatePrice1Min(results)
+		self.__updatePrice(results)
 
+	def close(self):
+		self.__conn.commit()
+		self.__conn.close()
 
-	def __createTables(self):
-		CREATE_TRADES_TABLE = 'CREATE TABLE IF NOT EXISTS TRADES ("amount" DOUBLE NOT NULL , "date" DATETIME NOT NULL , "globalTradeID" INTEGER PRIMARY KEY  NOT NULL  UNIQUE , "rate" DOUBLE NOT NULL , "total" DOUBLE NOT NULL , "tradeID" INTEGER NOT NULL  UNIQUE , "type" BOOL NOT NULL )'
-		CREATE_PRICE_1MIN_TABLE = 'CREATE TABLE IF NOT EXISTS PRICE_1MIN ("date" INT PRIMARY KEY NOT NULL UNIQUE, "open" DOUBLE NOT NULL , "high" DOUBLE NOT NULL , "low" DOUBLE NOT NULL, "close" DOUBLE NOT NULL , "volume" DOUBLE NOT NULL)'
-		self.__cursor.execute( CREATE_TRADES_TABLE )
-		self.__cursor.execute( CREATE_PRICE_1MIN_TABLE )
+	def abort(self):
+		self.__conn.close()
+
+	def __pullTradeHistory(self, startTime, endTime):
+
+		history = []
+		poloniexAPI = poloniexClient.PoloniexClient('', '')
+		i = 0
+		for thisStart in range( startTime , endTime, 3600):
+			req = {'currencyPair': self.currencyPair, 'start': thisStart, 'end': thisStart + 3599}
+			result = poloniexAPI.api_query('returnMarketTradeHistory',req) 
+			if( type(result) != list):
+				print "Error: ", result
+				break
+			else:
+				history = history + result
+			time.sleep(.25)
+			i += 1
+		return history
 
 	def __updateTrades(self, trades):
 		for trade in trades:
@@ -102,17 +127,15 @@ class PoloniexDAO():
 			total         = 	float( trade['total'] )
 			tradeID       = 	int( trade['tradeID'] )
 			tradeType     = 	1 if trade['type'] == 'buy' else 0
-
 			try:
-	 
-				SQL_INSERT = " INSERT OR REPLACE INTO TRADES VALUES ( ?, ?, ?, ?, ?, ?,? ) "
+				SQL_INSERT = " INSERT OR REPLACE INTO TRADES VALUES ( ?,  ?, ?, ?, ?, ?, ? ) "
 				self.__cursor.execute( SQL_INSERT, [amount, date, globalTradeID, rate, total, tradeID, tradeType])
 
 			except sqlite3.Error as detail:
 				print( detail , " ", SQL_INSERT)
 				pass
 
-	def __updatePrice1Min(self, trades):
+	def __updatePrice(self, trades):
 
 		if ( len(trades) > 0 ):
 			# Convert to DataFrame 
@@ -136,30 +159,47 @@ class PoloniexDAO():
 			# Create 1Min volume sums
 			volume = volume.resample('1Min', how='sum')
 
-			price1Min = pandas.concat([price,volume], axis=1)
-			price1Min = price1Min.interpolate()
-			print price1Min
-
-			for index, row in price1Min.iterrows():
-				timeInt = int(time.mktime(time.strptime(str(index), "%Y-%m-%d %H:%M:%S")))
-				SQL_INSERT = " INSERT OR REPLACE INTO PRICE_1Min VALUES ( ?, ?, ?, ?, ?, ? ) "
-				self.__cursor.execute( SQL_INSERT, [ timeInt, row['open'], row['high'], row['low'], row['close'], row['amount']])
+			price = pandas.concat([price,volume], axis=1)
+			price['open'] = price['open'].fillna(method='ffill')
+			price['high'] = price['high'].fillna(method='ffill')
+			price['low'] = price['low'].fillna(method='ffill')
+			price['close'] = price['close'].fillna(method='ffill')
+			price['amount'] = price['amount'].fillna(0)
 
 
-	def close(self):
-		self.__conn.commit()
-		self.__conn.close()
+			for index, row in price.iterrows():
+				SQL_INSERT = " INSERT OR REPLACE INTO PRICE VALUES ( ?, ?, ?, ?, ?, ? ) "
+				self.__cursor.execute( SQL_INSERT, [ str(index), row['open'], row['high'], row['low'], row['close'], row['amount']])
 
-	def abort(self):
-		self.__conn.close()
+
+def toSeconds(date):
+	return int(time.mktime(time.strptime( str(date) , "%Y-%m-%d %H:%M:%S")))
+
+
+
+
+
+dao = PoloniexDAO("/Users/JRS/Data/", "BTC_XCP")
+dao.dropTables()
+dao.update()
+print dao.selectMaxTime()
+print dao.selectMinTime()
+print dao.selectAllPriceHistory()
+print dao.selectAllTradeHistory()
+print dao.selectTradeHistory('2016-07-30 04:09:00','2016-07-30 04:25:00' )
+print dao.selectPriceHistory('2016-07-30 04:09:00','2016-07-30 04:25:00' )
+dao.close()
 
 
 dao = PoloniexDAO("/Users/JRS/Data/", "BTC_ETH")
+dao.dropTables()
 dao.update()
 print dao.selectMaxTime()
-# dao.update('2016-07-20 00:55:00', '2016-07-30 01:00:00')
-# print dao.selectAllPrice1MinHistory()
-print dao.selectPrice1MinHistory('2016-07-30 04:00:00','2016-07-30 05:00:00' )
+print dao.selectMinTime()
+print dao.selectAllPriceHistory()
+print dao.selectAllTradeHistory()
+print dao.selectTradeHistory('2016-07-30 04:09:00','2016-07-30 04:25:00' )
+print dao.selectPriceHistory('2016-07-30 04:09:00','2016-07-30 04:25:00' )
 dao.close()
 
 
