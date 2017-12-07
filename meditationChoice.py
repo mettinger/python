7c5c5ebd842c01d57a6b8dcff897f9b6713e4e2d
Mark Ettinger <jettinger35@gmail.com>
2017-12-07 05:25:58 -0500

    message

---
+++ meditationChoice.py
@@ -0,0 +1,6 @@
+#!/Users/marke/anaconda/bin/python
+
+import numpy as np
+
+meditations = ['ground in sensation', 'reality checks', 'simulation yoga/epoche','push/pull','sense of being','ego motivation']
+print np.random.choice(meditations)
