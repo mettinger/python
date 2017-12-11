64393508b20c0c8335b2e5b0e3b25a41bd076624
Mark Ettinger <jettinger35@gmail.com>
2017-12-11 08:45:21 -0500

    message

--- meditationChoice.py
+++ meditationChoice.py
@@ -2,5 +2,5 @@
 
 import numpy as np
 
-meditations = ['ground in sensation', 'reality checks', 'simulation yoga/epoche','push/pull','sense of being','ego motivation']
+meditations = ['ground in sensation', 'reality checks', 'simulation yoga/epoche','push/pull','sense of being','ego motivation','acceptance']
 print np.random.choice(meditations)
