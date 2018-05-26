a516f50ea622a18b785a4885eb6471b06df534be
mettinger <jettinger35@gmail.com>
2018-05-26 22:14:31 +0000

    message

---
+++ buttonApp_2/app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
+from flask_login import LoginManager
+
+app = Flask(__name__)
+
+login = LoginManager(app)
+login.login_view = 'login'
+
+
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models
---
+++ buttonApp_2/app/models.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+class ButtonData(db.Model):
+    # CREATE TABLE button_press (timestamp text, button_id int, info text)
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.String(50), index=True)
+    button_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    info = db.Column(db.String(300))
+    
+    def __repr__(self):
+        return '<ButtonData {}>'.format(self.info)
+    
+    
+    
+'''
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
+'''
