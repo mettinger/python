f0ea04d5927025ebdea4f5f379115cf3e424c91d
mettinger <jettinger35@gmail.com>
2018-06-01 18:00:46 +0000

    message

--- buttonApp_2/config.py
+++ buttonApp_2/config.py
@@ -6,3 +6,22 @@ class Config(object):
     SECRET_KEY = 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    '''
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']
+    '''
+    
+    MAIL_SERVER='localhost'
+    MAIL_PORT=8025
+    MAIL_USE_TLS=0
+    MAIL_USERNAME=''
+    MAIL_PASSWORD=''
+    ADMINS = ['jettinger35@gmail.com']
+    
+    
+    
