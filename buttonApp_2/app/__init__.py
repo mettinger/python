408a8902ded06afcbe86167e131d9c2b3152e004
mettinger <jettinger35@gmail.com>
2018-06-01 12:45:49 +0000

    message

--- buttonApp_2/app/__init__.py
+++ buttonApp_2/app/__init__.py
@@ -3,8 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
 
 login = LoginManager(app)
 login.login_view = 'login'
