4a6c45693599a95e55cfe3db4b5451f741436275
mettinger <jettinger35@gmail.com>
2020-09-01 10:25:45 -0400

    message

---
+++ maintain.py
@@ -0,0 +1,16 @@
+# auto move and click
+
+import pyautogui
+import time
+import random
+
+xLim,yLim = pyautogui.size()
+
+while True:
+    newX = xLim/2 + random.randint(0,50)
+    newY = yLim/2 + random.randint(0,60)
+    pyautogui.moveTo(newX, newY, duration = .5)
+    pyautogui.dragTo(newX, newY, duration = .5, button='left') 
+    secondsToSleep = (60 * 5) + random.randint(3, 60)
+    #secondsToSleep = 5
+    time.sleep(secondsToSleep)
