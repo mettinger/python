358e06705167c3f49d42103f5a01579eb511c6d7
mettinger <jettinger35@gmail.com>
2020-09-14 13:22:49 -0400

    message

--- maintain.py
+++ maintain.py
@@ -6,11 +6,12 @@ import random
 
 xLim,yLim = pyautogui.size()
 
+avgMinutesSleep = 7
+
 while True:
     newX = xLim/2 + random.randint(0,50)
     newY = yLim/2 + random.randint(0,60)
     pyautogui.moveTo(newX, newY, duration = .5)
     pyautogui.dragTo(newX, newY, duration = .5, button='left') 
-    secondsToSleep = (60 * 5) + random.randint(3, 60)
-    #secondsToSleep = 5
+    secondsToSleep = (60 * avgMinutesSleep) + random.randint(3, 60)
     time.sleep(secondsToSleep)
