3deae2ed6fee5803de97e32f89077c7588ca4c47
mettinger <jettinger35@gmail.com>
2022-02-03 13:04:58 -0800

    message

--- eeg/pyvistaTest.py
+++
@@ -1,7 +0,0 @@
-from pyvista import examples
-import time
-
-mesh = examples.download_dragon()
-mesh['scalars'] = mesh.points[:, 1]
-mesh.plot(cpos='xy', cmap='plasma',pbr=True, metallic=1.0, roughness=0.6,zoom=1.7)
-
